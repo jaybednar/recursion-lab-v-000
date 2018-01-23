e545fd7f8f6f8f64e299f40b447c5438b8c0b7c6
Jay Bednar <ironviralmedia@gmail.com>
2018-01-23 18:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function reverseString(string) {
   if (string.length > 1) {
     mySubString = string.substring((string.length - 1));
     reverseString(mySubString);
+  } else {
+    return true;
   }
 }
