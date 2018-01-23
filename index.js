81cdd1142d8319934dbab8b6dfd492ea28a56509
Jay Bednar <ironviralmedia@gmail.com>
2018-01-23 18:42:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
